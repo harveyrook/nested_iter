e98b4da61b97b4b484e16bcdf0d004154338a156
HarveyDev <harveyrook@hotmail.com>
2021-11-30 10:54:02 -0800

    switched from flatten to flat_map

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 //
 // This is an excercise to teach myself how to use nested iterators 
 // instead of a nested for loop, and then do some combined computation
-// with each of the loop indexes. The idea is to map the outer iterator
-// to the inner iterator. Since iterators are lazy, you must flatten
-// and for_each the result. You must also move the data since the
+// with each of the loop indexes. The idea is to flat_map the outer iterator
+// to the inner iterator. Since iterators are lazy, you must
+// for_each the result. You must also move the data since the
 // iterator can be executed after the parent is gone.
 //
 // Why do this? Iterators handle borrows more cleanly than expanded
@@ -18,8 +18,7 @@ fn main() {
     let vec2 = vec![5, 6, 7, 8, 9];
 
     vec1.iter()
-        .map( |y| vec2.iter()
+        .flat_map( |y| vec2.iter()
                       .map(move |z| 10*y+z ))
-        .flatten()
         .for_each(move |a| println!("Value {0}",a));
 }
